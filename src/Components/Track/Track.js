ab4e00da9c198177e4a1a48a5a5f3857a373e13d
AT-LEGEND <atlegend0@gmail.com>
2020-12-12 01:19:13 +0530

    small change made (< step 35)

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Track.css";
 class Track extends React.Component {
+	constructor(props) {
+		super(props);
+		this.renderAction = this.renderAction.bind(this);
+	}
 	renderAction() {
 		if (this.props.isRemoval) {
 			<button className='Track-action'>-</button>;
@@ -12,9 +16,9 @@ class Track extends React.Component {
 		return (
 			<div className='Track'>
 				<div className='Track-information'>
-					<h3>{/*<!-- track name will go here -->*/}</h3>
+					<h3>{this.props.track.name}</h3>
 					<p>
-						{/*<!-- track artist will go here--> | <!-- track album will go here -->*/}
+						{this.props.track.artist} | {this.props.track.album}
 					</p>
 				</div>
 				{this.renderAction()}
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./TrackList.css";
 import Track from "../Track/Track";
+
 class TrackList extends React.Component {
 	render() {
 		return (
 			<div className='TrackList'>
-				{/*<!-- You will add a map method that renders a set of Track components  -->*/}
+				{this.props.tracks.map((track) => {
+					return <Track key={track.id} track={track} />;
+				})}
 			</div>
 		);
 	}
 }
+
 export default TrackList;
