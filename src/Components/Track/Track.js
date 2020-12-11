796799a44b4f23318807df43a216bb4dc68a3417
AT-LEGEND <atlegend0@gmail.com>
2020-12-11 19:21:38 +0530

    Static components set (step 28)

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Track.css";
+class Track extends React.Component {
+	renderAction() {
+		if (this.props.isRemoval) {
+			<button className='Track-action'>-</button>;
+		} else {
+			<button className='Track-action'>+</button>;
+		}
+	}
+	render() {
+		return (
+			<div className='Track'>
+				<div className='Track-information'>
+					<h3>{/*<!-- track name will go here -->*/}</h3>
+					<p>
+						{/*<!-- track artist will go here--> | <!-- track album will go here -->*/}
+					</p>
+				</div>
+				{this.renderAction()}
+			</div>
+		);
+	}
+}
+export default Track;
