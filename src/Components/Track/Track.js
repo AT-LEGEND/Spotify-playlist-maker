776804176467ae2a545cd5877f1d2df63b3bd453
AT-LEGEND <atlegend0@gmail.com>
2020-12-13 20:57:41 +0530

    Add tracks to playlist

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,13 +4,21 @@ class Track extends React.Component {
 	constructor(props) {
 		super(props);
 		this.renderAction = this.renderAction.bind(this);
+		this.addTrack = this.addTrack.bind(this);
 	}
 	renderAction() {
 		if (this.props.isRemoval) {
-			<button className='Track-action'>-</button>;
+			return <button className='Track-action'>-</button>;
 		} else {
-			<button className='Track-action'>+</button>;
+			return (
+				<button className='Track-action' onClick={this.addTrack}>
+					+
+				</button>
+			);
 		}
+	}
+	addTrack() {
+		this.props.onAdd(this.props.track);
 	}
 	render() {
 		return (
