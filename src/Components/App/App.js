3559c39ec4004d9f83910b8c1452a58ac9c92040
AT-LEGEND <atlegend0@gmail.com>
2020-12-14 01:59:24 +0530

    change playlist name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -52,6 +52,7 @@ class App extends React.Component {
 		};
 		this.addTrack = this.addTrack.bind(this);
 		this.removeTrack = this.removeTrack.bind(this);
+		this.updatePlaylistName = this.updatePlaylistName.bind(this);
 	}
 	addTrack(track) {
 		let tracks = this.state.playlistTracks;
@@ -65,6 +66,11 @@ class App extends React.Component {
 		let tracks = this.state.playlistTracks;
 		tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
 		this.setState({ playlistTracks: tracks });
+	}
+	updatePlaylistName(name) {
+		this.setState({
+			playlistName: name,
+		});
 	}
 	render() {
 		return (
@@ -83,6 +89,7 @@ class App extends React.Component {
 							playlistName={this.state.playlistName}
 							playlistTracks={this.state.playlistTracks}
 							onRemove={this.removeTrack}
+							onNameChange={this.updatePlaylistName}
 						/>
 					</div>
 				</div>
